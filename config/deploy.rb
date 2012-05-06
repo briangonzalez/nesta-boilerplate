9d6906d319316859a9fd56dfedb2a0434e7aa55d
Brian Gonzalez <brian.gonzalez@geni.com>
2012-05-06 10:53:54 -0700

    enabling cache on prod and ensuring rack starts as specified user in cap deploy file

--- config/deploy.rb
+++ config/deploy.rb
@@ -32,6 +32,7 @@ namespace :deploy do
 
   task :finalize_update, :except => { :no_release => true } do
     run "chmod -R g+w #{latest_release}" if fetch(:group_writable, true)
+    run "chown #{user}:#{user} -R #{latest_release}" if fetch(:group_writable, true)
 
     # mkdir -p is making sure that the directories are there for some SCM's that don't save empty folders
     run <<-CMD
