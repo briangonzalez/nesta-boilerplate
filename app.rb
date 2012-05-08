3236464707250087d06ac693cbceb3114a39efab
Brian Gonzalez <brian.gonzalez@geni.com>
2012-05-08 08:51:02 -0700

    adding app and lib

---
+++ app.rb
@@ -0,0 +1,28 @@
+require 'compass'
+require 'compass-h5bp'
+require 'haml'
+require 'sass'
+require 'yaml'
+Dir["lib/**/*.rb"].each {|f| require "./#{f}"}
+
+module Nesta
+  class App
+    nesta_config = YAML::load(File.open(File.join("config", "config.yml")))
+    use Rack::Static, :urls => ['/' + nesta_config['theme']], :root => 'themes/' + nesta_config['theme'] + '/public'
+    
+    configure do
+      # Default Haml format is :xhtml. Let's change that.
+      set :haml, { :format => :html5 }
+    end
+
+    configure :production do
+      set :haml, { :ugly => true }
+    end
+
+    get '/css/:sheet.css' do
+      content_type 'text/css', :charset => 'utf-8'
+      cache sass(params[:sheet].to_sym, Compass.sass_engine_options)
+    end
+
+  end
+end
